57c47493c0c3c0b5898aa2c390076ced14ddb4c3
Parthasaradhi <parthasaradhi.karnam@in.bosch.com>
2021-04-04 22:28:25 +0530

    Fixing Review Comments

--- src/main/java/sensorval/SensorValidator.java
+++ src/main/java/sensorval/SensorValidator.java
@@ -3,7 +3,7 @@ package sensorval;
 import java.util.List;
 
 public class SensorValidator {
-	public static boolean isMaxDelta(double value, double nextValue, double maxDelta) {
+	public static boolean ifThreshold(double value, double nextValue, double maxDelta) {
 
 		if (nextValue - value > maxDelta) {
 			return false;
@@ -12,31 +12,31 @@ public class SensorValidator {
 	}
 
 	public static boolean validateSOCreadings(List<Double> values) {
-		return getReadings(values, 0.5);
+		return maxDeltaCheck(values, 0.5);
 	}
 
 	public static boolean validateCurrentreadings(List<Double> values) {
-		return getReadings(values, 0.1);
+		return maxDeltaCheck(values, 0.1);
 	}
 
-	public static boolean getReadings(List<Double> values, double maxDelta) {
+	public static boolean maxDeltaCheck(List<Double> values, double maxDelta) {
 		int lastButOneIndex = getLastButOneIndex(values);
 		for (int i = 0; i < lastButOneIndex; i++) {
-			if(!getMaxDeltaCheck(values, i, maxDelta)) {
+			if (!getMaxDeltaCheck(values, i, maxDelta)) {
 				return false;
 			}
 
 		}
 		return true;
 	}
-	
+
 	public static boolean getMaxDeltaCheck(List<Double> values, int index, double maxDelta) {
-		if (!nullCheck(values, index) && !isMaxDelta(values.get(index), values.get(index + 1), maxDelta)) {
+		if (!nullCheck(values, index) && !ifThreshold(values.get(index), values.get(index + 1), maxDelta)) {
 			return false;
-		} 
+		}
 		return true;
 	}
-	
+
 	public static boolean nullCheck(List<Double> values, int index) {
 		if (values.get(index) == null || values.get(index + 1) == null) {
 			return true;
